cdf878380f8f4526cf15fe5cc78854f0d5d61d31
Michael Rivera <soymrmike@gmail.com>
2019-05-18 22:04:59 -0700

    Client does load gists from server.

--- public/index.jsx
+++ public/index.jsx
@@ -1,12 +1,78 @@
+class AjaxAssistant {
+
+  constructor($) {
+    this.$ = $
+  }
+
+  handleError(xhr, reject) {
+    var text = ""
+    try {
+      const data = JSON.parse(xhr.responseText)
+      text = data.errors
+    } catch(e) {
+      text = xhr.responseText
+    }
+
+    if(text === "") {
+      if(xhr.status === 404) {
+        text = "A resource could not be found."
+      }
+    }
+    
+    reject(text)
+  }
+  
+  post(path, data) {
+    return new Promise((resolve, reject) => {
+      if(!data) data = {}
+      this.$.ajax({
+        method: 'POST',
+        url: path,
+        dataType: 'JSON',
+        data: data,
+        beforeSend: (xhr) => { xhr.setRequestHeader('CSRF-Token', this.$('meta[name=csrf-token]').attr('content')) },
+        success: (data) => resolve(data),
+        error: (xhr) => this.handleError(xhr, reject)
+      })
+    })
+  }
+  
+  get(path) {
+    return new Promise((resolve, reject) => {
+      this.$.ajax({
+        url: path,
+        dataType: 'JSON',
+        success: (data) => resolve(data),
+        error: (xhr) => this.handleError(xhr, reject)
+      })
+    })
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      gists: null
+    }
+
+  }
+
+  componentDidMount() {
+    if(!this.state.gists) {
+      new AjaxAssistant($).get('/api/gists')
+        .then(gists => this.setState({gists: gists}))
+    }
+  }
+
+  gistUrl(gist) {
+    return `https://gist.github.com/${this.props.username}/${gist.vendor_id}`
   }
   
   render() {
-    const gists = this.props.gists ? this.props.gists.map(function(g) { return (
+    const gists = this.state.gists ? this.state.gists.map((g) => { return (
       <tr key={g.id}>
-        <th>{g.title}</th><th><a href={g.url}>{g.url}</a></th>
+        <td>{g.title}</td><td><a href={this.gistUrl(g)} target="_blank">{g.vendor_id}</a></td>
       </tr>
     )}) : ""
 
